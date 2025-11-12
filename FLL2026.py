1be5a502ba6364e2d7b7a7976d1b6a6a1965f635
Dorai Habil <habildorai2009@gmail.com>
2025-11-12 18:30:51 +0100

    Add files via upload

---
+++ FLL2026.py
@@ -0,0 +1,40 @@
+import asyncio
+import json
+import websockets
+
+pc_clients = set()
+flutter_clients = set()
+
+async def handle_pc(websocket):
+    print("[INFO] PC connected")
+    pc_clients.add(websocket)
+    try:
+        async for message in websocket:
+            for client in flutter_clients:
+                await client.send(message)
+    finally:
+        pc_clients.remove(websocket)
+        print("[INFO] PC disconnected")
+
+async def handle_flutter(websocket):
+    print("[INFO] Flutter connected")
+    flutter_clients.add(websocket)
+    try:
+        async for _ in websocket:
+            pass
+    finally:
+        flutter_clients.remove(websocket)
+        print("[INFO] Flutter disconnected")
+
+async def main(websocket, path):
+    if path == "/pc":
+        await handle_pc(websocket)
+    elif path == "/flutter":
+        await handle_flutter(websocket)
+    else:
+        await websocket.close()
+
+print("[INFO] Render server running...")
+start_server = websockets.serve(main, "0.0.0.0", 10000)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
